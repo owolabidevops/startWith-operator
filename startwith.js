6d3ecdef16d98f877553890c4cfb8fe0bec8ddf3
Owolabi <48913682+owolabidevops@users.noreply.github.com>
2020-04-18 13:17:21 +0100

    Add files via upload

---
+++ startwith.js
@@ -0,0 +1,4 @@
+const str='hello word,welcome to the javascript';
+console.log(str.startsWith('hello'));//true
+console.log(str.startsWith('help'));//false
+// it tells you if a given string start with a particular substring
